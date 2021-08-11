1beb5e226b5929caa4ba6bf0ed15345372e9d21b
carllos95 <carlos95rodrigo@hotmail.com>
2021-08-11 19:11:18 -0300

    Subindo arquivos página sobre - Desktop e responsivo

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ListarSobre from './components/about/about';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ListarSobre />
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
