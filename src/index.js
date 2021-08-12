24f5a73f3fc25feaa1b9e114bf98d58c3916421b
Ezequiel Frizon <61747201+zeeckcwb@users.noreply.github.com>
2021-08-11 21:59:32 -0300

    inserindo arquivos index.js e index.css na branch

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Header from './components/header/header';
-import Main from './components/main/main'
-import Footer from './components/footer/footer'
+import Main from './components/main/main';
+import Footer from './components/footer/footer';
 
 ReactDOM.render(
   <React.StrictMode>
