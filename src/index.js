f8ac60e40c8f5136366160c17a2abe72953ecc98
Ezequiel Frizon <61747201+zeeckcwb@users.noreply.github.com>
2021-08-12 18:42:14 -0300

    Inserindo na branch develop os arquivos atuais da branch Ezequiel

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Header from './components/header/header';
+import Main from './components/main/main';
+import Footer from './components/footer/footer';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Header />
+    <Main />
+    <Footer />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
