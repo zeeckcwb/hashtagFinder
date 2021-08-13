904bb0fb18494c272d2b484dcaa810f72e96a9c1
Rhaissa Marques <rhaissa.marques@gmail.com>
2021-08-13 10:55:30 -0300

    subindo as funções renderMain, renderSair e botões

--- src/components/Historic/historic.jsx
+++ src/components/Historic/historic.jsx
@@ -1,35 +1,56 @@
 import React, {useEffect, useState} from 'react';
 import "./historic.css";
-import imgHome from "./Versão 2/icon-home.svg";
-import imgOut from "./Versão 2/icon-power-off.svg";
-//import logo from "./Versão 2/logo.svg";
+import ReactDOM from 'react-dom';
+import Main from '../../components/main/main';
+import Header from '../header/header';
+import Footer from '../footer/footer';
+import logo from '../../images/logo.png';
+import iconHome from '../../images/icon-home.svg';
+import iconPowerOff from '../../images/icon-power-off.svg';
+import Login from '../login/Login';
+
+
+
+function renderMain(){
+  ReactDOM.render(
+      <React.StrictMode>
+          <Header/>
+          <Footer/>
+          <Main/>
+      </React.StrictMode>,
+      document.getElementById('root')
+  );
+}
+
+function renderSair(){
+  ReactDOM.render(
+      <React.StrictMode>
+          <Header/>
+          <Login/>
+      </React.StrictMode>,
+      document.getElementById('root')
+  );
+}
 
 
 export default function Historic() {
     return (
        <>
        <link href="https://fonts.googleapis.com/css2?family=Rubik&display=swap" rel="stylesheet"></link>
+          
+            <section className="header">
+              <div className="headerContent">
+                  <div className="headerLogo">
+                      <img src={logo} alt="Logo hashtagfinder" />
+                  </div>
+                  <div className="headerButtons">
+                      <button onClick={renderMain} className="buttonLight"><img src={iconHome} alt="icone home"/>HOME</button>
+                      <button onClick={renderSair} className="buttonDark"><img src={iconPowerOff} alt="icone power off"/>SAIR</button>
+                  </div>
+              </div>
+            </section>
+          
           <div className='body'>
-            <div className='header'>
-              <div className='hashtagFinderLogo'>
-                <div className='hashtagLogo'>
-                  hashtag
-                </div>
-                <div className='finderLogo'>
-                  finder
-                </div>
-              </div>
-              <div className='homeLogout'>
-                <div className='home'>
-                <img className='imageHome' src={imgHome} alt="logo"/>
-                  HOME
-                </div>
-                <div className='logout'>
-                <img className='imageOut' src={imgOut} alt="ImageOut"/>
-                  SAIR
-                </div>
-              </div>
-            </div>
             <div className='container'>
               <h2>Buscas realizadas</h2>
               <div className='list'>
@@ -94,54 +115,12 @@ export default function Historic() {
                     <div className='times'>09:30</div>
                   </div>
                 </div>
-                <div className='listItems' id='thisThis'>
-                  <div className='hashtagName'>#hashtagname</div>
-                  <div className='datesTimes'>
-                    <div className='dates'>25/02</div>
-                    <div className='times'>09:30</div>
-                  </div>
-                </div>
-                <div className='listItems' id='thisThis'>
-                  <div className='hashtagName'>#hashtagname</div>
-                  <div className='datesTimes'>
-                    <div className='dates'>25/02</div>
-                    <div className='times'>09:30</div>
-                  </div>
-                </div>
-                <div className='listItems' id='thisThis'>
-                  <div className='hashtagName'>#hashtagname</div>
-                  <div className='datesTimes'>
-                    <div className='dates'>25/02</div>
-                    <div className='times'>09:30</div>
-                  </div>
-                </div>
-                <div className='listItems' id='thisThis'>
-                  <div className='hashtagName'>#hashtagname</div>
-                  <div className='datesTimes'>
-                    <div className='dates'>25/02</div>
-                    <div className='times'>09:30</div>
-                  </div>
-                </div>
-                <div className='listItems' id='thisThis'>
-                  <div className='hashtagName'>#hashtagname</div>
-                  <div className='datesTimes'>
-                    <div className='dates'>25/02</div>
-                    <div className='times'>09:30</div>
-                  </div>
-                </div>
-                <div className='listItems' id='thisThis'>
-                  <div className='hashtagName'>#hashtagname</div>
-                  <div className='datesTimes'>
-                    <div className='dates'>25/02</div>
-                    <div className='times'>09:30</div>
-                  </div>
-                </div>
+                
               </div>
 
               </div>
             </div>
           </div>
-          <br className='footer'></br>
         </> 
     );
   }
