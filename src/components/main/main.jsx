98d38a3bbca3806b4505681569a5e893dbf5532b
Ezequiel Frizon <61747201+zeeckcwb@users.noreply.github.com>
2021-08-23 17:43:34 -0300

    Inserida a função de carregamento das ultimas 10 imagens, validação do campo, tratamento da hashtag informada removendo o # caso houver

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,47 +1,53 @@
 import './main.css'
 import { useEffect, useState } from 'react'
 import axios from "axios"
-import nature1 from '../../images/provisionally/1.jpg'
-import nature2 from '../../images/provisionally/2.jpg'
-import nature3 from '../../images/provisionally/3.jpg'
-import nature4 from '../../images/provisionally/4.jpg'
 
 export default function Main(){
 
     /*Função utilizada para alternar as abas Tweets e Imagens no mobile*/
     function showImages(){
-        document.getElementById("postResultsImages").style.display = 'flex';
-        document.getElementById("postResultsText").style.display = 'none';
+        document.getElementById("postResultsImages").style.display = 'flex'
+        document.getElementById("postResultsText").style.display = 'none'
         document.getElementById("selectImages").classList.add("active")
         document.getElementById("selectTweets").classList.remove("active")
     }
 
     /*Função utilizada para alternar as abas de Tweets e Imagens no mobile*/
     function showText(){
-        document.getElementById("postResultsText").style.display = 'block';
-        document.getElementById("postResultsImages").style.display = 'none';
+        document.getElementById("postResultsText").style.display = 'block'
+        document.getElementById("postResultsImages").style.display = 'none'
         document.getElementById("selectTweets").classList.add("active")
         document.getElementById("selectImages").classList.remove("active")
     }
 
     let [tweets, setTweets] = useState([])
-    var teste = [1, 2, 3, 4, 5]
+    let [images, setImages] = useState([])
 
     function getTweets(){
-        let hashtag = document.getElementById('enter').value
+        let hashtag = document.getElementById('enter').value.replace(/#/g, "")
         axios.get('https://cors.bridged.cc/https://api.twitter.com/1.1/search/tweets.json?q='+hashtag+'', {
             method: 'GET',
             headers: {
                 Authorization: 'Bearer AAAAAAAAAAAAAAAAAAAAAFlKHgEAAAAApBW4nRyRkiogluzAbXlS4KuHlMU%3DFcR7r8N19LRnMHLVmYlFsod6Be6zUvZD2rxATotl6mLPAh2UEX'
             },
-        }).then((resp) => {setTweets(resp.data.statuses)
-        console.log(tweets)})
-        //https://api.twitter.com/1.1/search/tweets.json?q=bmw&count=10
+        }).then((resp) => {setTweets(resp.data.statuses)})
+    }
+
+    function getImages(){
+        let hashtag = document.getElementById('enter').value.replace(/#/g, "")
+        axios.get('https://cors.bridged.cc/https://api.twitter.com/2/tweets/search/recent?query='+hashtag+'%20has:images&max_results=30&expansions=author_id,attachments.media_keys&media.fields=type,url,width,height', {
+            method: 'GET',
+            headers: {
+                Authorization: 'Bearer AAAAAAAAAAAAAAAAAAAAAFlKHgEAAAAApBW4nRyRkiogluzAbXlS4KuHlMU%3DFcR7r8N19LRnMHLVmYlFsod6Be6zUvZD2rxATotl6mLPAh2UEX'
+            },
+        }).then((resp) => {setImages(resp.data.includes.media)})
+        console.log(images)
     }
 
     const handler = (event) => {
         if (event.key === 'Enter') {
            getTweets();
+           getImages()
         }
      };
 
@@ -77,55 +83,36 @@ export default function Main(){
                     </div>
                     <div className="postResults">
                         <div id="postResultsImages" className="postResultsImages">
-                            <div className="imageContainer">
-                                <div className="imageContent" style={{backgroundImage: `url(${nature1})`}}> 
+                        {images.slice(0, 10).map((i, index) => {
+                            return (
+                                <div className="imageContainer" key={index}>
+                                    <div className="imageContent" style={{backgroundImage: `url(${i.url})`}}>
+                                    </div>
+                                    <div className="textContent">
+                                        <p>Postado por:</p>
+                                        <p>@username</p>
+                                    </div>
                                 </div>
-                                <div className="textContent">
-                                    <p>Postado por:</p>
-                                    <p>@twitterusername</p>
-                                </div>
-                            </div>
-                            <div className="imageContainer">
-                                <div className="imageContent" style={{backgroundImage: `url(${nature2})`}}>
-                                </div>
-                                <div className="textContent">
-                                    <p>Postado por:</p>
-                                    <p>@twitterusername</p>
-                                </div>
-                            </div>
-                            <div className="imageContainer">
-                                <div className="imageContent" style={{backgroundImage: `url(${nature3})`}}>
-                                </div>
-                                <div className="textContent">
-                                    <p>Postado por:</p>
-                                    <p>@twitterusername</p>
-                                </div>
-                            </div>
-                            <div className="imageContainer">
-                                <div className="imageContent" style={{backgroundImage: `url(${nature4})`}}>
-                                </div>
-                                <div className="textContent">
-                                    <p>Postado por:</p>
-                                    <p>@twitterusername</p>
-                                </div>
-                            </div>
+                            )
+                        })}
                         </div>
+
                         <div id="postResultsText" className="postResultsText">
                         {tweets.slice(0, 10).map((t, index) => {
                             return (
                                 <div className="textContainer" key={index}>
-                                <div className="userInfos">
-                                    <div className="userThumb" style={{backgroundImage: `url(${t.user.profile_image_url})`}}>
+                                    <div className="userInfos">
+                                        <div className="userThumb" style={{backgroundImage: `url(${t.user.profile_image_url})`}}>
+                                        </div>
+                                        <div className="userText">
+                                            <p>{t.user.name} <span>@{t.user.screen_name}</span></p>
+                                            <p>{t.text}</p>
+                                        </div>
                                     </div>
-                                    <div className="userText">
-                                        <p>{t.user.name} <span>@{t.user.screen_name}</span></p>
-                                        <p>{t.text}</p>
+                                    <div className="seeMore">
+                                        <a href={'https://twitter.com/'+t.user.screen_name+'/status/'+t.id_str} target="_blank">Ver mais no Twitter</a>
                                     </div>
                                 </div>
-                                <div className="seeMore">
-                                    <a href={'https://twitter.com/'+t.user.screen_name+'/status/'+t.id_str} target="_blank">Ver mais no Twitter</a>
-                                </div>
-                            </div>
                             )
                         })}
                         </div>
