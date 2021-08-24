268931f654a0a36b86b2e67dec4920c427757a2f
Rhaissa Marques <rhaissa.marques@gmail.com>
2021-08-24 20:15:49 -0300

    atualização do conteúdo da branch rhaissa para a branch develop

--- src/components/Historic/historic.jsx
+++ src/components/Historic/historic.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import "./historic.css";
 import ReactDOM from 'react-dom';
 import Main from '../../components/main/main';
@@ -34,9 +34,32 @@ function renderSair(){
 }
 
 
-export default function Historic() {
+/* Função para listar as buscas realizadas */
+function HistoricSearch(){
+  
+  const [data, setData] = useState([]);
+  const [itensPerPage, setItensPerPage] = useState(10); //setando 10 ítens por página
+  const [currentPage, setCurrentPage] = useState(0); //página inicial 0
+
+  const pages = Math.ceil(data.length / itensPerPage); 
+  const startIndex = currentPage * itensPerPage; 
+  const endIndex = startIndex + itensPerPage; 
+  const currentItens = data.slice(startIndex, endIndex); 
+
+  useEffect(() => {
+
+    fetch("https://api.airtable.com/v0/app6wQWfM6eJngkD4/tbl4mrtX1Owvos7eB?filterByFormula=(%7BSquad%7D+%3D+'5')&maxRecords=100&pageSize=100&sort%5B0%5D%5Bfield%5D=Data&sort%5B0%5D%5Bdirection%5D=asc&sort%5B1%5D%5Bfield%5D=Hora&sort%5B1%5D%5Bdirection%5D=desc&timeZone=America/Sao_Paulo&api_key=key2CwkHb0CKumjuM")
+      
+      .then(resp => resp.json())
+      .then(data => {
+        setData(data.records);
+        
+      })
+  })
+
+
     return (
-       <>
+      <>
        {/* Import da fonte */}
        <link href="https://fonts.googleapis.com/css2?family=Rubik&display=swap" rel="stylesheet"></link>
           
@@ -59,6 +82,11 @@ export default function Historic() {
               {/* Título da página */}
               <h2>Buscas realizadas</h2>
 
+              <div className='buttonsPages'>{Array.from(Array(pages), (item, index) => { //criando os botões para passar o conteúdo, 10 por vez até mostrar todo o conteúdo
+                  return <button style= { index === currentPage ? {backgroundColor: "#0A1744", color: "#72EFDB"} : null} value={index} onClick={(e) => setCurrentPage(Number(e.target.value))}>{index + 1}</button>
+                } )}
+              </div>
+
               {/* Cabeçalho da lista */}
               <div className='list'>
 
@@ -74,61 +102,28 @@ export default function Historic() {
 
               {/* Conteúdo da lista */}
               <div className='listBackground'>
-                <div className='listItems'>
-                  <div className='hashtagName'>#hashtagname</div>
-                  <div className='datesTimes'>
-                    <div className='dates'>25/02</div>
-                    <div className='times'>09:30</div>
-                  </div>
-                </div>
-                <div className='listItems'>
-                  <div className='hashtagName'>#hashtagname</div>
-                  <div className='datesTimes'>
-                    <div className='dates'>25/02</div>
-                    <div className='times'>09:30</div>
-                  </div>
-                </div>
-                <div className='listItems'>
-                  <div className='hashtagName'>#hashtagname</div>
-                  <div className='datesTimes'>
-                    <div className='dates'>25/02</div>
-                    <div className='times'>09:30</div>
-                  </div>
-                </div>
-                <div className='listItems'>
-                  <div className='hashtagName'>#hashtagname</div>
-                  <div className='datesTimes'>
-                    <div className='dates'>25/02</div>
-                    <div className='times'>09:30</div>
-                  </div>
-                </div>
-                <div className='listItems'>
-                  <div className='hashtagName'>#hashtagname</div>
-                  <div className='datesTimes'>
-                    <div className='dates'>25/02</div>
-                    <div className='times'>09:30</div>
-                  </div>
-                </div>
-                <div className='listItems'>
-                  <div className='hashtagName'>#hashtagname</div>
-                  <div className='datesTimes'>
-                    <div className='dates'>25/02</div>
-                    <div className='times'>09:30</div>
-                  </div>
-                </div>
-                <div className='listItems'>
-                  <div className='hashtagName'>#hashtagname</div>
-                  <div className='datesTimes'>
-                    <div className='dates'>25/02</div>
-                    <div className='times'>09:30</div>
-                  </div>
-                </div>
+                {currentItens.map(
+                  item =>
+                  (
+
+                    <div className='contentList' key={item.id}>
+                      <div className={"listHashtag"}>{item.fields.Hashtag}</div>
+                      <div className={"contentBodyList"}>
+                        <div className={"listDate"}>{item.fields.Data}</div>
+                        <div className={"listHour"}>{item.fields.Hora}</div>
+                      </div>
+                    </div>
+
+                  )
+                )}
                 
               </div>
 
-              </div>
             </div>
           </div>
-        </> 
+        </div>
+      </> 
     );
-  }
+}
+
+export default HistoricSearch;
